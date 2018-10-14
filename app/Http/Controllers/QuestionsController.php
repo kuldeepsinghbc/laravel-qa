2d4e6f3982cf5e2fb04cf2b13b065093abf5da29
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-10-14 11:03:23 -0700

    Implement authorizations using Policies

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -15,6 +15,7 @@ class QuestionsController extends Controller
     public function index()
     {
         // \DB::enableQueryLog();
+
         $questions =Question::with('user')->latest()->paginate(5);
         return view('questions.index',compact('questions'));
         // view('questions.index',compact('questions'))->render();
