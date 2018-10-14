fca7e0fd49a1390b43dc8eb3869830a5ae593909
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-10-14 10:57:16 -0700

    Implement authorizaton using policies

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,11 +8,10 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth',['except' =>['index','show']]);
+    }
     public function index()
     {
         // \DB::enableQueryLog();
@@ -69,6 +68,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize("update", $question);
         // $question = Question::findOrFail($id);
         return view("questions.edit", compact('question'));
     }
@@ -82,6 +82,7 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize("update", $question);
         $question->update($request->only('title','body'));
         return redirect('/questions')->with('success','Your question has been updated.');
     }
@@ -94,6 +95,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize("delete", $question);
         $question->delete();
         return redirect('/questions')->with('success', 'Your question has been deleted.');
     }
