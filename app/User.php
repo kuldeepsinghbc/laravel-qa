324683238491cd8f7cb5474cd59519ceb8c7d865
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-10-13 17:08:03 -0700

    corrected User model

--- app/User.php
+++ app/User.php
@@ -28,7 +28,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
     public function questions()
-    [
+    {
         return $this->hasMany(Question::class);
-    ];
+    }
 }
