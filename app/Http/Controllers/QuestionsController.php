e0a5490b92b097891330e40dd345d42d58de8627
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-10-14 10:23:56 -0700

    Implement authorization using Gates

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -69,8 +69,13 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        // $question = Question::findOrFail($id);
+        if(\Gate::denies('update-question', $question)){
+            abort(403,"Access denied");
+        }
+
         return view("questions.edit", compact('question'));
+        // $question = Question::findOrFail($id);
+
     }
 
     /**
@@ -82,6 +87,9 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        if (\Gate::denies('update-question', $question)) {
+            abort(403, "Access denied");
+        }
         $question->update($request->only('title','body'));
         return redirect('/questions')->with('success','Your question has been updated.');
     }
@@ -94,6 +102,9 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        if (\Gate::denies('delete-question', $question)) {
+            abort(403, "Access denied");
+        }
         $question->delete();
         return redirect('/questions')->with('success', 'Your question has been deleted.');
     }
