1c314f19427cf7fd7c56a2a92ea3d93f4a26899f
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-10-16 11:19:52 -0700

    Display answes for questions

---
+++ app/Http/Controllers/AnswersController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Answer;
+use App\Question;
+use Illuminate\Http\Request;
+
+class AnswersController extends Controller
+{
+
+
+    public function store(Question $question, Request $request)
+    {
+        $question->answers()->create($request->validate([
+            'body' => 'required'
+        ]) + ['user_id' => \Auth::id()]);
+        return back()->with('success', "Your answer has been submitted successfully");
+
+    }
+
+
+    public function edit(Answer $answer)
+    {
+        //
+    }
+
+    public function update(Request $request, Answer $answer)
+    {
+        //
+    }
+    public function destroy(Answer $answer)
+    {
+        //
+    }
+}
