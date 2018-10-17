6a02ac7a18715471b1e880c1011fcb6ca9674cfa
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-10-16 17:01:27 -0700

    Update best_answer_id on questions table when an answer removed

--- app/Answer.php
+++ app/Answer.php
@@ -26,12 +26,22 @@ class Answer extends Model
             $answer->question->increment('answers_count');
         });
         static::deleted(function ($answer){
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if($question->best_answer_id === $answer->id)
+            {
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
     }
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+    public function getStatusAttribut()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 
 }
