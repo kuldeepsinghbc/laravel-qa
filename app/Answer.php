bf27d8c5313523728fd8c52a7591825ae36eb4f0
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-10-16 22:08:25 -0700

    Implement accept the answer as best answer

--- app/Answer.php
+++ app/Answer.php
@@ -33,9 +33,18 @@ class Answer extends Model
     {
         return $this->created_at->diffForHumans();
     }
-    public function getStatusAttribut()
+    public function getStatusAttribute()
     {
-        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+        return $this->isBest ? 'vote-accepted' : '';
     }
+    public function getIsBestAttribute()
+    {
+        return $this-> isBest();
+    }
+    public function isBest()
+    {
+        return $this->id === $this->question->best_answer_id;
+    }
+
 
 }
