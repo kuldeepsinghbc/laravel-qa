63eec5d6b2838864962392d5b35cbd17c5203832
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-10-20 11:28:02 -0700

    Beautify the flash message and confirm dialog

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,10 @@ require('./bootstrap');
 require('./fontawesome');
 
 window.Vue = require('vue');
+import VueIziToast from 'vue-izitoast';
+import 'izitoast/dist/css/izitoast.min.css';
+
+Vue.use(VueIziToast);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
